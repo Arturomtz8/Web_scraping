d9af7b3c241f435c3bffa0b55b815c49fdaf7a07
Arturomtz8 <arturo.mtz8@gmail.com>
2021-05-14 13:20:34 -0500

    second

---
+++ bauhaus.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+#Obtener los id's para usarlos en el buscador
+"""df = pd.read_excel('KW19.xlsx')
+df['ids'] = df['url'].str[:8]
+df.to_excel("prueba.xlsx", encoding="utf_8_sig", index=False)"""
+
+
+
+
+"""df = pd.read_excel('KW19.xlsx')
+df['match']=df.ids.str.endswith(';').map({True:'Y',False:'N'})
+df.to_excel("comp.xlsx", encoding="utf_8_sig", index=False)"""
+
+
+
+# Get the ids of the products to search them in a web page.
+df = pd.read_excel("KW19.xlsx")
+list_of_ids = df['Ref'].to_list()
+list_of_ids = [str(x) for x in list_of_ids]
+
+
+# Search on the web and save the results in a list
+final_titles = []
+final_urls = []
+for num in list_of_ids:
+    search_url = "https://www.bauhaus.es/buscar/productos?text=" + num
+    response = requests.get(search_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    prod_tags = soup.select('h3.product-list-tile__info__name')
+
+    titles = [tag.text for tag in prod_tags]
+    urls = [link.a.get("href") for link in prod_tags]
+    final_titles.append("".join(titles))
+    final_urls.append("".join(urls))
+
+
+print('Finish')
+
+#Delete empty spaces that can affect the url and add the domain
+final_titles = [x.strip() for x in final_titles]
+final_urls = [y.strip() for y in final_urls]
+final_urls = ['https://www.bauhaus.es' + y for y in final_urls]
+
+
+#Save it to a new excel
+df2 = pd.DataFrame(list(zip(final_titles, final_urls)), columns =['Nombre de producto', 'urls'])
+df2.to_excel('lista de productos.xlsx', encoding="utf_8_sig", index=False)
